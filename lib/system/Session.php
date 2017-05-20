7292ffd414a9e340feab0e9cbd5aa87d0344f581
scubillos <cbasqbll95@hotmail.com>
2017-05-19 22:37:36 -0500

    Correccion de Sesion

--- lib/system/Session.php
+++ lib/system/Session.php
@@ -2,11 +2,12 @@
 namespace Base;
 
 class Session{
-	public function __construct(){}
+	public function __construct(){
+		session_start();
+	}
 	
 	public function verifySession(){
-		session_start();
-		if(!isset($_SESSION["idbin"])){
+		if(!isset($_SESSION["idhex"])){
 			session_destroy();
 			return false;
 		}
@@ -14,24 +15,20 @@ class Session{
 	}
 	
 	public function destroySession(){
-		session_start();
 		
 		session_destroy();
 		return true;
 	}
 	
 	public function varSession_set($nameVar,$valueVar){
-		session_start();
 		$_SESSION[$nameVar] = $valueVar;
 	}
 	
 	public function varSession_get($nameVar){
-		session_start();
 		return $_SESSION[$nameVar];
 	}
 	
 	public function getVarsSession(){
-		@session_start();
 		$varsSession = [];
 		foreach($_SESSION as $key => $value){
 				$varsSession[$key] = $value;
