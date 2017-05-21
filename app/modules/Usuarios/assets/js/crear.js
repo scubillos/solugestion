141822e3e73d4a0a4d5831a276747e1d4ca78526
jhoncardenas11 <jhoncardenas11@gmail.com>
2017-05-20 19:52:40 -0500

    correccion de diseño y validacion

--- app/modules/Usuarios/assets/js/crear.js
+++ app/modules/Usuarios/assets/js/crear.js
@@ -4,10 +4,39 @@ jQuery.extend(jQuery.validator.messages, {
 $(function() {
     $( "#CrearForm" ).validate( {
 		rules: {
+			"campo[nit]":{
+				required: true,
+				number: true
+			},
+
+			"campo[telefono]":{
+				number: true
+			},
+
+			"campo[telpercontac]":{
+				number: true
+			},
+
+			"campo[numovil]":{
+				number: true
+			},
+
+			"campo[mail]":{
+				email: true
+			},
+
+			rpass:{
+				equalTo: pass
+			}
 
 		},
 		messages: {
-			
+			"campo[nit]": "Este campo debe ser un número",
+			"campo[telefono]": "Este campo debe ser un número",
+			"campo[telpercontac]": "Este campo debe ser un número",
+			"campo[numovil]": "Este campo debe ser un número",
+			"campo[mail]": "Ingrese una direccion de correo valida",
+			rpass: "Las contraseñas no coinciden"
 		},
 		errorElement: "em",
 		errorPlacement: function ( error, element ) {
