f8cbfa7f89ea0d726abe2feeae30ea317a44e0d3
scubillos <cbasqbll95@hotmail.com>
2017-05-24 00:18:08 -0500

    Formulario creacion parametros Diagnostico SG-SST

---
+++ app/modules/AdmDiagnosticoSG_SST/assets/js/crear.js
@@ -0,0 +1,60 @@
+/*jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio."
+});
+$(function() {
+    $( "#CrearForm" ).validate( {
+		rules: {
+			"campo[nit]":{
+				required: true,
+				number: true
+			},
+
+			"campo[telefono]":{
+				number: true
+			},
+
+			"campo[num_percontacto]":{
+				number: true
+			},
+
+			"campo[num_movil]":{
+				number: true
+			},
+
+			"campo[correo]":{
+				email: true
+			},
+
+			rpass:{
+				equalTo: pass
+			}
+
+		},
+		messages: {
+			"campo[nit]": "Este campo debe ser un número",
+			"campo[telefono]": "Este campo debe ser un número",
+			"campo[num_percontacto]": "Este campo debe ser un número",
+			"campo[num_movil]": "Este campo debe ser un número",
+			"campo[correo]": "Ingrese una direccion de correo valida",
+			rpass: "Las contraseñas no coinciden"
+		},
+		errorElement: "em",
+		errorPlacement: function ( error, element ) {
+			// Add the `help-block` class to the error element
+			error.addClass( "help-block" );
+
+			if ( element.prop( "type" ) === "checkbox" ) {
+				error.insertAfter( element.parent( "label" ) );
+			} else {
+				error.insertAfter( element );
+			}
+		},
+		highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+		},
+		unhighlight: function (element, errorClass, validClass) {
+			$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+		}
+	} );
+
+});*/
---
+++ app/modules/AdmDiagnosticoSG_SST/assets/js/tabla.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+	//Se definen elementos del DOM
+	var elem = {
+		idTable: "#TablaAdmParamDiagnostico",
+		pagerTable: "#TablaAdmParamDiagnosticoPager",
+		botonOpciones: ".viewOptions",
+		modalOpciones: "#ModalOpciones",
+		botonEditar: "#btnEditar",
+		botonEliminar: "#btnEliminar"
+	};
+	
+	//jqGrid
+	$(elem.idTable).jqGrid({
+		url: baseUrl() + 'AdmDiagnosticoSG_SST/listar',
+		mtype: "post",
+		styleUI : 'Bootstrap',
+		datatype: "json",
+		colNames: ["Numeral","Marco legal","Criterio","Modo de verificación","",""],
+		colModel: [
+			{ name: 'numeral', width: 75 },
+			{ name: 'marco_legal', width: 75 },
+			{ name: 'criterio', width: 75 },
+			{ name: 'modo_verificacion', width: 75 },
+			{ name: '', width: 50 },
+			{ name: 'link', width: 150, hidden:true }
+		],
+		height: "auto",
+		width: $(elem.idTable).parent().width() - 50,
+		rowList: [10, 20,50, 100],
+		rowNum: 10,
+		page: 1,
+		pager: elem.pagerTable,
+		loadtext: '<p>Cargando...',
+		beforeRequest: function(data, status, xhr){},
+		align:"center",
+		viewrecords: true
+	});
+	
+	//Modal opciones
+	$(elem.idTable).on("click",elem.botonOpciones,function(e){
+		e.preventDefault();
+		e.returnValue=false;
+		e.stopPropagation();
+		
+		var id = $(this).data("id");
+		var rowINFO = $(elem.idTable).getRowData(id);
+		var options = rowINFO["link"];
+		
+		$(elem.modalOpciones).find('.modal-title').empty().html('Catalogo: <b>'+ rowINFO["numeral"]+rowINFO["marco_legal"] +'</b>');
+		$(elem.modalOpciones).find('.modal-body').empty().html(options);
+		$(elem.modalOpciones).find('.modal-footer').empty();
+		$(elem.modalOpciones).modal('show');
+	});
+	//Eliminar
+	$(elem.modalOpciones).on("click",elem.botonEliminar,function(e){
+		e.preventDefault();
+		e.returnValue=false;
+		e.stopPropagation();
+		
+		var id = $(this).data("id");
+		
+		$.ajax({
+			url: baseUrl() + "AdmCatalogos/eliminar",
+			type:"post",
+			async:false,
+			data: { id: id },
+			success:function(response,status){
+				var data = $.parseJSON(response);
+				if(data.finish == true){
+					Toast(data.message, data.status, data.title);
+					$(elem.modalOpciones).modal("hide");
+				}
+			}
+		});
+	});
+});
