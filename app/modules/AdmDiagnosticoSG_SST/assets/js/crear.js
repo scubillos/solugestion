8d17953cc9464b553e5b96d9d698c3f6f73cd589
scubillos <cbasqbll95@hotmail.com>
2017-05-25 00:04:51 -0500

    Avance Diagnostico SG

--- app/modules/AdmDiagnosticoSG_SST/assets/js/crear.js
+++ app/modules/AdmDiagnosticoSG_SST/assets/js/crear.js
@@ -1,60 +1,34 @@
-/*jQuery.extend(jQuery.validator.messages, {
-    required: "Este campo es obligatorio."
-});
+
 $(function() {
-    $( "#CrearForm" ).validate( {
-		rules: {
-			"campo[nit]":{
-				required: true,
-				number: true
-			},
-
-			"campo[telefono]":{
-				number: true
-			},
-
-			"campo[num_percontacto]":{
-				number: true
-			},
-
-			"campo[num_movil]":{
-				number: true
-			},
-
-			"campo[correo]":{
-				email: true
-			},
-
-			rpass:{
-				equalTo: pass
+    var elem = {
+		paso:"#paso",
+		seccion:"#seccion",
+		subseccion:"#subseccion"
+	}
+	var tags = {
+		optionVacio:'<option value="">Seleccione</option>'
+	};
+	
+	$(elem.paso).change(function(e){
+		var id_paso = $(this).val();
+		
+		$.ajax({
+			url: baseUrl() + "AdmDiagnosticoSG_SST/ajax_buscarSecciones",
+			type:"post",
+			async:false,
+			data: { id_paso: id_paso },
+			success:function(response,status){
+				var data = $.parseJSON(response);
+				if(data.finish == true){
+					var options = data.options;
+					$(elem.seccion).html(tags.optionVacio);
+					_.forEach(options,function(option){
+						var tagOption = '<option value="'+option.id+'">'+option.texto+'</option>';
+						$(elem.seccion).append(tagOption);
+					});
+				}
 			}
+		});
+	});
 
-		},
-		messages: {
-			"campo[nit]": "Este campo debe ser un número",
-			"campo[telefono]": "Este campo debe ser un número",
-			"campo[num_percontacto]": "Este campo debe ser un número",
-			"campo[num_movil]": "Este campo debe ser un número",
-			"campo[correo]": "Ingrese una direccion de correo valida",
-			rpass: "Las contraseñas no coinciden"
-		},
-		errorElement: "em",
-		errorPlacement: function ( error, element ) {
-			// Add the `help-block` class to the error element
-			error.addClass( "help-block" );
-
-			if ( element.prop( "type" ) === "checkbox" ) {
-				error.insertAfter( element.parent( "label" ) );
-			} else {
-				error.insertAfter( element );
-			}
-		},
-		highlight: function ( element, errorClass, validClass ) {
-			$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
-		},
-		unhighlight: function (element, errorClass, validClass) {
-			$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
-		}
-	} );
-
-});*/
+});
